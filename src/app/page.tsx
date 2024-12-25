f82b88e61d9763859833843be3b6f4846dbb48df
Arnav <arnav.verma2022@vitstudent.ac.in>
2024-12-26 02:38:03 +0530

    Add utility functions and UI components with Tailwind CSS integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,337 @@
 import Image from "next/image";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Github, Linkedin, Mail, Phone } from "lucide-react";
 
 export default function Home() {
+  const experience = {
+    title: "Frontend Developer (Freelance)",
+    company: "VR Food Products",
+    link: "https://vrfoodproducts.com",
+    repo: "https://github.com/pcell62/vrfoodproducts",
+    description: [
+      "Developed a fully responsive e-commerce website with React.js, Next.js, Tailwind CSS, and TypeScript, improving user engagement by 40%.",
+      "Designed catalog, landing, about us, and product pages to showcase 50+ products with detailed descriptions.",
+      "Deployed on Hostinger, achieving 99.9% uptime and reducing load times by 30%.",
+      "Enhanced performance to score 90 on Google Lighthouse for desktop.",
+    ],
+  };
+  const projects = [
+    {
+      title: "AD-Assist",
+      subtitle: "College Add and Drop Platform",
+      link: "https://ad-eta-vert.vercel.app",
+      repo: "https://github.com/Arnav17v/ad",
+      description: [
+        "Built a platform to facilitate class slot and teacher exchanges within college communities using Next.js, Tailwind CSS, and Firebase.",
+        "Designed and implemented features including request posting, contact sharing, and full CRUD functionality.",
+        "Integrated Firebase Authentication for secure user login and access.",
+        "Deployed on Vercel, ensuring seamless performance and high availability.",
+      ],
+    },
+    {
+      title: "Sumo Snack",
+      subtitle: "Japanese Cuisine Restaurant Platform",
+      link: "https://sumo-snack.vercel.app",
+      repo: "https://github.com/Arnav17v/sumo-snack",
+      description: [
+        "Developed a Japanese cuisine restaurant platform showcasing 20+ menu items with a minimalist, elegant design using Next.js and Tailwind CSS.",
+        "Created a main landing page, menu page, and order online page, improving user engagement by 30% through intuitive navigation.",
+        "Implemented features like menu sorting options, search functionality, and a cart system for seamless checkout, reducing user task time by 25%.",
+      ],
+    },
+  ];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="md:h-screen md:flex md:items-center md:justify-center">
+      <div className="overflow-hidden relative sm:flex-grow-0 sm:w-fit sm:h-lvh">
+        <div className="p-5 md:pr-[3rem]">
+          <div className="pb-4">
+            <div className="text-3xl">hi I'm</div>
+            <div className="text-8xl max-w-fit md:text-[10rem]">
+              <span className="px-2">Arnav</span> <br />{" "}
+              <span className="bg-col2 text-col5 px-2 rounded-xl">Verma</span>
+            </div>
+          </div>
+          <div className="text-2xl mt-3 max-w-[30rem]">
+            Hello and Welcome to my portfolio. Here are some quick links you
+            would be interested in 👇👇👇👇
+          </div>
+          <div className="grid grid-cols-2 text-col5 gap-1 mt-10">
+            <div className="py-6 flex justify-center items-center text-3xl bg-col1 rounded-xl">
+              <a href="#projects">Projects</a>
+            </div>
+            <div className="py-6 flex justify-center items-center text-3xl bg-col1 rounded-xl">
+              <a href="#skills">Skills</a>
+            </div>
+            <a href="https://drive.google.com/file/d/1Nkp__o5EQzXfaApJqFapxRaPLL3r5-3a/view?usp=sharing">
+              <div className="py-6 flex justify-center items-center text-3xl bg-col1 rounded-xl">
+                Resume
+              </div>
+            </a>
+            <div className="py-6 flex justify-center items-center text-3xl bg-col1 rounded-xl">
+              <a href="#contact">Contact Me</a>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div className="md:absolute md:rotate-90 md:top-[442px] md:left-[-408px] h-fit">
+          <div className="marquee-text bg-col2 py-2">
+            <div className="text-xl font-bold marquee-text-track">
+              <p>·</p>
+              <p>Projects</p>
+              <p>·</p>
+              <p>Skills</p>
+              <p>·</p>
+              <p>Contact Me</p>
+              <p>·</p>
+              <p>Projects</p>
+              <p>·</p>
+              <p>Projects</p>
+              <p>·</p>
+              <p>Skills</p>
+              <p>·</p>
+              <p>Contact Me</p>
+              <p>·</p>
+              <p>Projects</p>
+              <p aria-hidden="true">·</p>
+              <p aria-hidden="true">Projects</p>
+              <p aria-hidden="true">·</p>
+              <p aria-hidden="true">Skills</p>
+              <p aria-hidden="true">·</p>
+              <p aria-hidden="true">Contact Me</p>
+              <p aria-hidden="true">·</p>
+              <p aria-hidden="true">Projects</p>
+              <p aria-hidden="true">·</p>
+              <p aria-hidden="true">Projects</p>
+              <p aria-hidden="true">·</p>
+              <p aria-hidden="true">Skills</p>
+              <p aria-hidden="true">·</p>
+              <p aria-hidden="true">Contact Me</p>
+              <p aria-hidden="true">·</p>
+              <p aria-hidden="true">Projects</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="overflow-y-scroll md:w-1/2 ml-0 flex-grow h-lvh ">
+        <div
+          className="md:text-8xl text-6xl pl-5 bg-col1 text-col5 py-5"
+          id="skills"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Skills
+          <div className="grid grid-cols-2 text-2xl sm:grid-cols-3 gap-6">
+            {/* Programming Languages */}
+            <div>
+              <h3 className="font-semibold">Programming Languages</h3>
+              <ul className="list-disc pl-4">
+                <li>Python</li>
+                <li>C++</li>
+                <li>TypeScript</li>
+              </ul>
+            </div>
+
+            {/* Frontend */}
+            <div>
+              <h3 className="font-semibold">Frontend</h3>
+              <ul className="list-disc pl-4">
+                <li>Next.js</li>
+                <li>React.js</li>
+                <li>Tailwind CSS</li>
+              </ul>
+            </div>
+
+            {/* Backend */}
+            <div>
+              <h3 className="font-semibold">Backend</h3>
+              <ul className="list-disc pl-4">
+                <li>Firebase</li>
+              </ul>
+            </div>
+
+            {/* Libraries & Tools */}
+            <div>
+              <h3 className="font-semibold">Libraries & Tools</h3>
+              <ul className="list-disc pl-4">
+                <li>NumPy</li>
+                <li>Pandas</li>
+                <li>Matplotlib</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="bg-col4 text-col5 text-xl font-bold w-full whitespace-nowrap overflow-hidden">
+          · ★ Projects ★ · ★ Projects ★ · ★ Projects ★ · ★ Projects ★ · ★
+          Projects ★ · ★ Projects ★ · ★ Projects ★ · ★ Projects ★ · ★ Projects ★
+          · ★ Projects ★ · ★ Projects ★ ·
+        </div>
+        <div className="md:text-8xl text-6xl ml-5 my-5" id="projects">
+          Projects
+          <section className="mt-5 text-xl">
+            <div className="grid gap-6">
+              {projects.map((project) => (
+                <Card
+                  className="bg-col5 hover:bg-col4 hover:text-col5 transition-all duration-500 border-4 mr-3 border-col4 shadow-none"
+                  key={project.title}
+                >
+                  <CardHeader>
+                    <CardTitle>
+                      <span className="flex  text-col1 items-center justify-between">
+                        {project.title}
+                        <div className="flex gap-2">
+                          <Button
+                            className="bg-col1 text-col5"
+                            variant="outline"
+                            size="sm"
+                            asChild
+                          >
+                            <Link href={project.link} target="_blank">
+                              Visit Site
+                            </Link>
+                          </Button>
+                          <Button
+                            className="bg-col1 text-col5"
+                            variant="outline"
+                            size="sm"
+                            asChild
+                          >
+                            <Link href={project.repo} target="_blank">
+                              <Github className="h-4 w-4" />
+                            </Link>
+                          </Button>
+                        </div>
+                      </span>
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="text-muted-foreground mb-4">
+                      {project.subtitle}
+                    </p>
+                    <ul className="list-disc pl-6 space-y-2">
+                      {project.description.map((point, index) => (
+                        <li key={index}>{point}</li>
+                      ))}
+                    </ul>
+                  </CardContent>
+                </Card>
+              ))}
+            </div>
+          </section>
+          <div className="text-5xl font-bold pt-3">Work Experience</div>
+          <section className="mt-1 text-xl">
+            <Card
+              className=" hover:bg-col4 hover:text-col5 transition-all duration-500 bg-col5  border-4 mr-3 border-col4 shadow-none"
+              key={experience.title}
+            >
+              <CardHeader>
+                <CardTitle>
+                  <span className="flex  text-col1 items-center justify-between">
+                    {experience.title}
+                    <div className="flex gap-2">
+                      <Button
+                        className="bg-col1 text-col5"
+                        variant="outline"
+                        size="sm"
+                        asChild
+                      >
+                        <Link href={experience.link} target="_blank">
+                          Visit Site
+                        </Link>
+                      </Button>
+                      <Button
+                        className="bg-col1 text-col5"
+                        variant="outline"
+                        size="sm"
+                        asChild
+                      >
+                        <Link href={experience.repo} target="_blank">
+                          <Github className="h-4 w-4" />
+                        </Link>
+                      </Button>
+                    </div>
+                  </span>
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-muted-foreground mb-4">
+                  {experience.company}
+                </p>
+                <ul className="list-disc pl-6 space-y-2">
+                  {experience.description.map((point, index) => (
+                    <li key={index}>{point}</li>
+                  ))}
+                </ul>
+              </CardContent>
+            </Card>
+          </section>
+        </div>
+        <div className="bg-col4 text-col5 text-xl font-bold w-full whitespace-nowrap overflow-hidden">
+          · ★ About ME ★ · ★ About ME ★ · ★ About ME ★ · ★ About ME ★ · ★ About
+          ME ★ · ★ About ME ★ · ★ About ME ★ · ★ About ME ★ · ★ About ME ★ · ★
+          About ME ★ · ★ About ME ★ ·
+        </div>
+        <div
+          className="md:text-8xl text-6xl pl-5 bg-col1 text-col5 py-5"
+          id="aboutme"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          About Me
+          <section className="text-3xl">
+            <h2 className="text-2xl font-semibold mb-4">Education</h2>
+            <div>
+              Bachelor of Technology in Computer Science and Engineering
+            </div>
+            <p className="text-muted-foreground">
+              Vellore Institute of Technology, Vellore
+            </p>
+            <p className="text-muted-foreground">Expected Graduation: 2026</p>
+            <p className="font-medium">CGPA: 8.97</p>
+          </section>
+        </div>
+        <div className="bg-col4 text-col5 text-xl font-bold w-full whitespace-nowrap overflow-hidden">
+          · ★ Contact ★ · ★ Contact ★ · ★ Contact ★ · ★ Contact ★ · ★ Contact ★
+          · ★ Contact ★ · ★ Contact ★ · ★ Contact ★ · ★ Contact ★ · ★ Contact ★
+          · ★ Contact ★ ·
+        </div>
+        <div className="md:text-8xl text-6xl ml-5 my-5" id="contact">
+          Contact Me{" "}
+          <section className="space-y-4">
+            <h1 className="text-4xl md:text-5xl font-bold tracking-tight">
+              Arnav Verma
+            </h1>
+            <p className="text-xl text-muted-foreground">
+              Computer Science Student & Developer
+            </p>
+            <div className="flex gap-4 flex-wrap">
+              <Button className="bg-col1 text-col5" variant="outline" asChild>
+                <Link href="mailto:arnavverma1204@gmail.com">
+                  <Mail className="mr-2 h-4 w-4" />
+                  Email
+                </Link>
+              </Button>
+              <Button className="bg-col1 text-col5" variant="outline" asChild>
+                <Link href="tel:+917814069496">
+                  <Phone className="mr-2 h-4 w-4" />
+                  Call
+                </Link>
+              </Button>
+              <Button className="bg-col1 text-col5" variant="outline" asChild>
+                <Link href="https://github.com/Arnav17v" target="_blank">
+                  <Github className="mr-2 h-4 w-4" />
+                  GitHub
+                </Link>
+              </Button>
+              <Button className="bg-col1 text-col5" variant="outline" asChild>
+                <Link
+                  href="https://www.linkedin.com/in/arnav17v"
+                  target="_blank"
+                >
+                  <Linkedin className="mr-2 h-4 w-4" />
+                  LinkedIn
+                </Link>
+              </Button>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
