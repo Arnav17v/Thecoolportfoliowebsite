e4e88766cf6d6dd488d3d7cf9fb07924236112fc
Arnav Verma <135861320+Arnav17v@users.noreply.github.com>
2026-05-12 00:51:48 +0530

    Update resume link to Google Drive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -145,7 +145,7 @@ export default function Home() {
             <div className="py-5 px-3 flex justify-center items-center text-2xl sm:text-3xl bg-col1 rounded-xl">
               <a href="#skills">Skills</a>
             </div>
-            <a href="/resume.pdf" target="_blank">
+            <a href="https://drive.google.com/file/d/1bWA9NRZA1VUmI6bYUKmwYMQxwT6x2prA/view?usp=sharing" target="_blank">
               <div className="py-5 px-3 flex justify-center items-center text-2xl sm:text-3xl bg-col1 rounded-xl">
                 Resume
               </div>
